88ac39c74a79a7cfeb5ccf6dd7b5ca1e1bff240a
johnfedoruk <johnny@johnfedoruk.ca>
2017-12-29 20:12:37 +0000

    Rendering cards of waterfall locations

--- src/app/pages/waterfalls/waterfalls.component.ts
+++ src/app/pages/waterfalls/waterfalls.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { WaterfallService } from "../../services/waterfall.service";
+import { Waterfall } from '../../models/waterfall';
 
 @Component({
-  selector: 'app-waterfalls',
-  templateUrl: './waterfalls.component.html',
-  styleUrls: ['./waterfalls.component.css']
+    selector: 'app-waterfalls',
+    templateUrl: './waterfalls.component.html',
+    styleUrls: ['./waterfalls.component.css']
 })
 export class WaterfallsComponent implements OnInit {
 
-  constructor() { }
+    public waterfalls:Waterfall[];
+    constructor(private waterfallService: WaterfallService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.waterfallService.waterfalls.subscribe(
+            waterfalls => this.waterfalls = waterfalls
+        );
+    }
 
 }
