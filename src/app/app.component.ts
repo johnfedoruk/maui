a8ec600e85d5b55a9c9975b8ad52ce326d0cb6fa
johnfedoruk <johnny@johnfedoruk.ca>
2017-12-29 23:59:49 +0000

    Scroll to top on NavigationEnd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterEvent, NavigationEnd } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    constructor(private router:Router) {}
+    public ngOnInit():void {
+        this.router.events.subscribe(
+            (event:RouterEvent) => {
+                if(event instanceof NavigationEnd)
+                    setTimeout(()=>document.body.scrollTop = 0,1)
+            }
+        )
+    }
+    title = 'app';
 }
