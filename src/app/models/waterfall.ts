ab1490793793c1e80d0b2f286bd0baed2673a34d
johnfedoruk <johnny@johnfedoruk.ca>
2017-12-29 20:02:18 +0000

    Added the Waterfall model

---
+++ src/app/models/waterfall.ts
@@ -0,0 +1,13 @@
+export interface Waterfall {
+    slug:string;
+    name:string;
+    description?:string;
+    height?:string;
+    access?:string;
+    tips?:string;
+    images:string[];
+    lat?:string;
+    lon?:string;
+    address?:string;
+    website?:string;
+}
