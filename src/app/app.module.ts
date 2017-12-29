2c4d1eca4d67c892c15d213b2b4573c54d0da21c
johnfedoruk <johnny@johnfedoruk.ca>
2017-12-29 18:31:47 +0000

    Added waterfalls pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Route } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { WaterfallsComponent } from './pages/waterfalls/waterfalls.component';
+
+const routes: Route[] = [
+    {
+        path:"",
+        redirectTo: "waterfalls",
+        pathMatch: "full"
+    },
+    {
+        path:"waterfalls",
+        component: WaterfallsComponent
+    }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WaterfallsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/waterfalls/waterfalls.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-waterfalls',
+  templateUrl: './waterfalls.component.html',
+  styleUrls: ['./waterfalls.component.css']
+})
+export class WaterfallsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
